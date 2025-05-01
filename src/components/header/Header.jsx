bf1aa13a2887ce11e030a083abe3cd502c282e8c
Philocles anais <anais_philocles@yahoo.fr>
2025-05-01 21:28:10 +0200

    update 3d texture and model three

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,26 +1,29 @@
-import React, { useRef } from 'react';
-import { Canvas, useFrame } from '@react-three/fiber';
-import { MeshTransmissionMaterial, Environment } from '@react-three/drei';
+import React, { useRef, useEffect, useMemo} from 'react';
+import { Canvas, useThree, useLoader,useFrame  } from '@react-three/fiber';
 import { Suspense } from 'react';
-import CarModel from './Car-gnx';
+import { TextureLoader } from 'three';
 import styled from 'styled-components';
-import Navbar from '../navbar/Navbar';
-import './header.css';
-import Butterfly from '/images/butterfly.png'
-import CreativeBallon from '/images/creative.png'
-import CreatBlack from '/images/c.png'
 import { OrbitControls } from '@react-three/drei';
+import * as THREE from 'three'; 
+import './header.css';
+import Navbar from '../navbar/Navbar'
 
-// Style du canvas
+//3D 
+import { MeshTransmissionMaterial, Environment } from '@react-three/drei';
+import CarModel from './Car-gnx';
+
+// Style du Canvas pour le rendre plein écran
 const StyledCanvas = styled(Canvas)`
-  width: 100%;
-  height: 50%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw; 
+  height: 100vh; 
+  z-index: 0;
+  display: block; 
+  pointer-events: none; 
 `;
 
-// Textes de base
 const textStyle = `
   position: absolute;
   font-family: "velista", serif;
@@ -30,111 +33,109 @@ const textStyle = `
   transform: translateY(0); 
   z-index: 1000;
 `;
-
-const PortfolioText = styled.h2`
-  ${textStyle}
-  top: 4%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  color: rgb(172, 170, 170);
-  font-size: 1rem;
-  font-weight: bold;
-  letter-spacing: 10px;
-`;
-
-const TextWebDeveloper = styled.h1`
-  ${textStyle}
-  top: 60%; 
-  left: 70%;
-  transform: translate(-50%, -50%);
-  color: rgb(180, 180, 174);
-  font-size: 5rem;
-`;
-
-
 const CreativeText = styled.p`
   ${textStyle}
   font-family: 'SankofaDisplay';
-  top: 50%;
-  left: 70%;
+  top: 55%;
+  left: 25%;
   transform: translate(-50%, -50%);
-  font-size: 8rem;
-  color: rgb(244, 244, 244);
+  font-size: 4rem;
+  color: rgb(225, 225, 225);
   
 `;
+const TextWebDeveloper = styled.h1`
+  ${textStyle}
+  top: 57%; 
+  left: 75%;
+  transform: translate(-50%, -50%);
+  color: rgb(180, 180, 174);
+  font-size: 2rem;
+`;
 
-// Cube (dans la scène 3D)
+
+const BackgroundPlane = () => {
+  const texture = useLoader(TextureLoader, '/images/Black-girl-hero.png'); 
+  const { viewport } = useThree();
+
+  // update image size
+  const scale = useMemo(() => {
+    const ratio = texture.image.width / texture.image.height;
+    const height = viewport.height;
+    const width = height * ratio; 
+    return [width, height, 1]; 
+  }, [texture, viewport]);
+
+  useEffect(() => {
+    // correct background image
+    texture.minFilter = THREE.LinearFilter;
+    texture.magFilter = THREE.LinearFilter;
+    texture.generateMipmaps = false;
+  }, [texture]);
+
+  return (
+    <mesh position={[0, -2, -10]} scale={scale}>
+      <planeGeometry args={[3, 3]}/>
+      <meshBasicMaterial map={texture} toneMapped={false} />
+    </mesh>
+  );
+};
+
+//CUBE 
 const Cube = () => {
   return (
-    <mesh position={[0, -0.2, 1]} scale={[1, 1, 1]}>
-      <boxGeometry args={[6, 2, 2]} />
+    <mesh position={[0, 1, 0]} scale={[1, 1, 2]}>
+      <boxGeometry args={[6, 1.5, 1]} />
       <MeshTransmissionMaterial
-        thickness={0.1}
         transmission={0.8}
         roughness={0.2}
         clearcoat={0.2}
-        envMapIntensity={0.2}
-        samples={4}
-        color="rgb(117, 117, 117)"
+        color="rgb(35, 37, 40)"
       />
     </mesh>
   );
 };
 
-// Scène contenant la voiture et le cube
- const Scene = () => {
-  const modelCarRef = useRef();
- 
+// SCENE
+const Scene = () => {
 
-  useFrame(() => {
-    if (modelCarRef.current) {
-      modelCarRef.current.rotation.y += 0.01;
-    }
-  });
+const modelCarRef = useRef();
 
+useFrame(() => {
+  if (modelCarRef.current) {
+    modelCarRef.current.rotation.y += 0.01;
+  }
+});
   return (
-    <Suspense fallback="loading...">
-       <Environment preset="city" intensity={0.4} />
+    <Suspense fallback={null}>
+      <BackgroundPlane />
+      <Environment preset="city" intensity={3}  background={false}/>
+      <ambientLight intensity={0.8} />
+      <directionalLight 
+        position={[5, 10, 5]} 
+        intensity={1}
+        color={"rgb(248, 226, 27)"} 
+        castShadow  
+      />
       <Cube />
-      <CarModel ref={modelCarRef} scale={[1, 1, 1]}  position={[-4, -2, -5]} />
-
+      <CarModel ref={modelCarRef} scale={[0.8, 0.8, 0.8]}  position={[-2, 0.45, -5]} />
     </Suspense>
-
-
   );
 };
 
-// Container pour la scène 3D
+// CANVA
 const CanvaContainer = () => {
-
   return (
-    <div className="backSection"> 
-    <div className="canva-section">
+    <div className="test">
       <Navbar/>
       <StyledCanvas>
         <Scene />
-        <ambientLight intensity={0.2} color={"rgb(108, 108, 108)"} />
-        <directionalLight 
-              position={[5, 10, 5]} 
-              intensity={5} 
-              color={"rgb(126, 129, 129)"} 
-              castShadow 
-             />
       </StyledCanvas>
-    
-      <div className="title-hero">
-        {/* <div className="butterfly">
-          <img src={CreatBlack} loading="lazy"  alt="papillon" />
-        </div> */}
-          <TextWebDeveloper>Developer</TextWebDeveloper>
-          <CreativeText>Creative</CreativeText>
-         
-     
-          {/* <PortfolioText>PORTFOLIO</PortfolioText> */}
+      <div className="Title-hero">
+        <TextWebDeveloper>Developer</TextWebDeveloper>
+        <CreativeText>Creative</CreativeText>
       </div>
     </div>
-    </div>
-
+     
   );
 };
 
